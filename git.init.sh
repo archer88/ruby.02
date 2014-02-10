9ccb674b7a21408c76df1f684ad4146b3c1982a4
archer88 <brad.matlack@gmail.com>
2014-02-09 19:43:33 -0700

    initial commit

--- git.init.sh
+++ git.init.sh
@@ -1,5 +1,13 @@
+git config --global user.name 'archer88'
+git config --global user.email 'brad.matlack@gmail.com'
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+git config --global color.ui true
+
 git init
 git add .
 git commit -m "initial commit"
 git mv README.rdoc README.md
 git commit -am "Improve the README"
+git remote add origin https://github.com/archer88/sample_app.git
+$ git push -u origin master
